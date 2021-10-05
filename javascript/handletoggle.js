7f3f5ec6f5f08d7fcfbfab5bc570acc11de84d32
JoravDu <53528905+JoravDu@users.noreply.github.com>
2021-10-05 14:38:22 +0200

    Initial commit

---
+++ javascript/handletoggle.js
@@ -0,0 +1,7 @@
+function handleToggle () {
+    const sidebarToggle = document.getElementById("sidebar");
+    sidebarToggle.classList.toggle("togglesidebar");
+
+    const contentToggle = document.getElementById("content");
+    contentToggle.classList.toggle("togglecontent")
+}
